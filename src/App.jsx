90fc8b2fa1db7279b84ac00b3bce1ee66b0977b4
Leonardo <leonardo0507.henrique@gmail.com>
2022-12-26 17:32:04 -0300

    Created all components React. HTML with JSX finished.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Header from "./components/Header";
 import Intro from "./components/Intro";
+import Footer from "./components/Footer";
 
 const App = () => {
     return (
         <>
             <Header />
             <Intro />
+            <Footer />
         </>
     );
 }
---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <footer>
+            <p>
+                <span>
+                    Desafio do 
+                </span>
+
+                <a href="https://www.frontendmentor.io/home"
+                target="_blank"
+                rel="external noopener noreferrer">
+                    Frontend Mentor
+                </a>.
+            </p>
+            <p>
+                <span>
+                    Desenvolvido por
+                </span>
+
+                <a href="https://www.linkedin.com/in/leonardo-henrique-/"
+                target="_blank"
+                rel="external noopener noreferrer">
+                    Leonardo Henrique
+                </a>.
+            </p>
+        </footer>
+    )
+}
+
+export default Footer
